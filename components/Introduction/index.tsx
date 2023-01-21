4045612b1f7966a7060d18a90d1de453f889bb0e
Fidelis Okeke <padrefidelis111@gmail.com>
2023-01-21 12:46:09 -0800

    refactor code

--- components/Introduction/index.tsx
+++ components/Introduction/index.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Block from "../Block";
 import TwoColumnGrid from "../TwoColumnGrid";
-import Styles from "./introduction.module.scss";
+import Styles from "./Introduction.module.scss";
 
 interface IIntroduction {
   title: string;
   subtitle: string;
   description: string;
 }
-export default function Introduction(props:IIntroduction) {
+export default function Introduction(props: IIntroduction) {
   return (
     <Block>
       <div className={Styles.introduction}>
@@ -16,15 +16,13 @@ export default function Introduction(props:IIntroduction) {
           <h4>{props.title}</h4>
           <TwoColumnGrid>
             <h4 className="light" style={{ marginRight: 100 }}>
-             {props.subtitle}
+              {props.subtitle}
             </h4>
             <Image src={"/images/Arrow.svg"} alt="" height={100} width={228} />
           </TwoColumnGrid>
         </div>
         <div style={{ maxWidth: "700px" }}>
-          <p>
-           {props.description}
-          </p>
+          <p>{props.description}</p>
         </div>
       </div>
     </Block>
